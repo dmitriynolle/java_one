1d17eb2f50c32c13cbbef1cbd3a8d086481f1b95
Dmitriy <dimusy@mail.ru>
2020-03-12 23:44:10 +0300

    first commit

---
+++ src/ru/geekbrains/java_one/Main.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_one;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/ru/geekbrains/java_one/Staff.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.java_one;
+
+public class Staff {
+}
